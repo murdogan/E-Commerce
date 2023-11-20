a37b51922f91bf8892e26f82cdcbf1f6e5317cfb
murdogan <mu.dogan44@gmail.com>
2023-11-20 13:49:17 +0300

    Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div>
+      <Header />
       <h1>Hello</h1>
-    </>
+    </div>
   );
 }
 
