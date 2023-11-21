16261418d033acca470974354af7809cd795d92c
murdogan <mu.dogan44@gmail.com>
2023-11-21 13:55:44 +0300

    Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import Categories from "./components/Categories/Categories";
+import Footer from "./components/Layout/Footer/Footer";
+import Header from "./components/Layout/Header/Header";
+import Policy from "./components/Layout/Policy/Policy";
+import Products from "./components/Products/Products";
+import Sliders from "./components/Slider/Sliders";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello</h1>
+      <Sliders />
+      <Categories />
+      <Products />
+      <Policy />
+      <Footer />
     </div>
   );
 }
