e16f72b311d5b322779d65260f681980ecff175a
murdogan <mu.dogan44@gmail.com>
2023-11-29 16:13:37 +0300

    Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 //import ProductDetailsPage from "./pages/ProductDetailsPage";
 //import BlogPage from "./pages/BlogPage";
 //import BlogDetailPage from "./pages/BlogDetailPage";
-import CartPages from "./pages/CartPages";
+// import CartPages from "./pages/CartPages";
 // import { ContactPage } from "./pages/ContactPage";
 //import AuthPage from "./pages/AuthPage";
-// import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 //import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
-    <React.Fragment>
-      {/* <HomePage /> */}
-      {/* <ShopPage /> */}
-      {/* <ContactPage /> */}
-      {/* <AuthPage /> */}
-      <CartPages />
-      {/* <BlogPage /> */}
-      {/* <BlogDetailPage /> */}
-      {/* <ProductDetailsPage /> */}
-    </React.Fragment>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
